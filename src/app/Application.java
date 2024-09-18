8fefedbb37827adc30f5439c9c4b7579731ff19c
Alisson Youssf <youssfbr@gmail.com>
2024-09-18 17:25:47 -0300

    Inicializar dados do Cliente

---
+++ src/app/Application.java
@@ -0,0 +1,18 @@
+package app;
+
+public class Application {
+    public static void main(String[] args) {
+
+        String nome = "Clark Kent";
+        String tipoConta = "Corrente";
+        double saldo = 1599.99;
+
+        System.out.println("\n***************************************");
+        System.out.println("\nNome do Cliente: " + nome);
+        System.out.println("Tipo da Conta: " + tipoConta);
+        System.out.println("Saldo Atual: " + saldo);
+
+        System.out.println("\n***************************************");
+
+    }
+}
