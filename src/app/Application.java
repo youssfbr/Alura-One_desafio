54944df05e716283531cbb332e7b5f154addf8b3
Alisson Youssf <youssfbr@gmail.com>
2024-09-18 18:03:15 -0300

    Funcionalidades (Visualizar , Enviar , Receber Saldo)

--- src/app/Application.java
+++ src/app/Application.java
@@ -32,6 +32,26 @@ public class Application {
             System.out.println(menu);
             opcao = sc.nextInt();
 
+            if (opcao == 1) {
+                System.out.println("O saldo atual é " + saldo);
+            } else if (opcao == 2) {
+                System.out.println("Digite o valor deseja transferir.");
+                double valorASerTransferido = sc.nextDouble();
+                if (valorASerTransferido > saldo) {
+                    System.out.println("Saldo insuficiente. Não é possível transferir.");
+                } else {
+                    saldo -= valorASerTransferido;
+                    System.out.println("Saldo atual: " + saldo);
+                }
+            } else if (opcao == 3 ) {
+                System.out.println("Digite o valor deseja recebido.");
+                double valorASerRecebido = sc.nextDouble();
+                saldo += valorASerRecebido;
+                System.out.println("Saldo atual: " + saldo);
+            } else if (opcao != 4) {
+                System.out.println("Opção inválida");
+            }
         }
+        System.out.println("Obrigado por usar nosso Sistema.");
     }
 }
